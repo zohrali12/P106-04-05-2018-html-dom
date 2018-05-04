8081060f8a77e61a3a4d973aa22e29a855fbb523
Samir Karimov <samir.karimov@code.edu.az>
2018-05-04 12:07:25 +0400

    simple plug-in + DOM

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+var elem = new IkiSetir(".box");
+elem.css({
+        width: "300px",
+        height: "500px",
+        background: "red"
+    })
+    .slideUp(1000);
+
+  
